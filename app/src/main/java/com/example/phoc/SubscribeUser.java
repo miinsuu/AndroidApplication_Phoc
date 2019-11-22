a0008762458cdcf0672c5cb1c84ad6be5e88b4ea
HamInGyu <hfire1207@gmail.com>
2019-11-23 02:48:48 +0900

    SubscribeUser에서 UserFeed로 전환 가능하게 만듬

--- app/src/main/java/com/example/phoc/SubscribeUser.java
+++ app/src/main/java/com/example/phoc/SubscribeUser.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 public class SubscribeUser extends Fragment implements View.OnClickListener{
 
     TextView fromSubscribeUser2ParticularTitle;
+    TextView fromSubscribeUser2UserFeed;
 
     @Nullable
     @Override
@@ -20,6 +21,10 @@ public class SubscribeUser extends Fragment implements View.OnClickListener{
 
         fromSubscribeUser2ParticularTitle = rootView.findViewById(R.id.fromSubscribeUser2ParticularTitle);
         fromSubscribeUser2ParticularTitle.setOnClickListener(this);
+
+        fromSubscribeUser2UserFeed = rootView.findViewById(R.id.fromSubscribeUser2UserFeed);
+        fromSubscribeUser2UserFeed.setOnClickListener(this);
+
         return rootView;
     }
 
@@ -27,6 +32,8 @@ public class SubscribeUser extends Fragment implements View.OnClickListener{
     public void onClick(View v) {
         if(v == fromSubscribeUser2ParticularTitle){
             ((main)getActivity()).onFragmentSelected(6,null);
+        } else if(v == fromSubscribeUser2UserFeed){
+            ((main)getActivity()).onFragmentSelected(7,null);
         }
     }
 }
