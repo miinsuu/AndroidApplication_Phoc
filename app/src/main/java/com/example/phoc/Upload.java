30f88e6428e793edfa918af648ecdc3b3c4c05dd
HamInGyu <hfire1207@gmail.com>
2019-11-23 00:33:42 +0900

    Upload 전시버튼 구현 완료

--- app/src/main/java/com/example/phoc/Upload.java
+++ app/src/main/java/com/example/phoc/Upload.java
@@ -1,14 +1,29 @@
 package com.example.phoc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Upload extends AppCompatActivity {
+public class Upload extends AppCompatActivity implements View.OnClickListener{
+
+    Button upload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        upload = findViewById(R.id.uploadBtn);
+        upload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == upload){
+            startActivity(new Intent(this, main.class));
+        }
     }
 }
