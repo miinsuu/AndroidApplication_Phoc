1fc7d56ea61f25e99714b097a7c2d9dd177f4950
HamInGyu <hfire1207@gmail.com>
2019-11-23 01:41:20 +0900

    titleList에서 particularTitle로 전환 가능하게 만듬

--- app/src/main/java/com/example/phoc/TitleList.java
+++ app/src/main/java/com/example/phoc/TitleList.java
@@ -4,17 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class TitleList extends Fragment{
+public class TitleList extends Fragment implements View.OnClickListener{
+
+    RelativeLayout fromTitleList2ParticularTitle;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.titlelist, container, false);
 
+        fromTitleList2ParticularTitle = rootView.findViewById(R.id.fromTitleList2ParticularTitle);
+        fromTitleList2ParticularTitle.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == fromTitleList2ParticularTitle){
+            ((main)getActivity()).onFragmentSelected(6,null);
+        }
     }
 }
