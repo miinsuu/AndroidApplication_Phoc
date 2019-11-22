e3baa5982f82b5a5728a9d1c7c94082a7f9ec5df
HamInGyu <hfire1207@gmail.com>
2019-11-23 01:57:02 +0900

    SubscribeUser에서 particularTitle로 전환 가능하게 만듬

--- app/src/main/java/com/example/phoc/SubscribeUser.java
+++ app/src/main/java/com/example/phoc/SubscribeUser.java
@@ -4,16 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SubscribeUser extends Fragment {
+public class SubscribeUser extends Fragment implements View.OnClickListener{
+
+    TextView fromSubscribeUser2ParticularTitle;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.subscribeuser, container, false);
 
+        fromSubscribeUser2ParticularTitle = rootView.findViewById(R.id.fromSubscribeUser2ParticularTitle);
+        fromSubscribeUser2ParticularTitle.setOnClickListener(this);
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == fromSubscribeUser2ParticularTitle){
+            ((main)getActivity()).onFragmentSelected(6,null);
+        }
     }
 }
