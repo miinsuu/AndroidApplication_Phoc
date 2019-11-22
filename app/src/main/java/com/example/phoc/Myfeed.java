5fffdf59a73f8a017c3b582888962f39b08534b0
HamInGyu <hfire1207@gmail.com>
2019-11-23 02:02:59 +0900

    Myfeed에서 particularTitle로 전환 가능하게 만듬

--- app/src/main/java/com/example/phoc/Myfeed.java
+++ app/src/main/java/com/example/phoc/Myfeed.java
@@ -4,17 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
-public class Myfeed extends Fragment {
+public class Myfeed extends Fragment implements View.OnClickListener{
+
+    TextView fromMyFeed2ParticularTitle;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.myfeed, container, false);
 
+        fromMyFeed2ParticularTitle = rootView.findViewById(R.id.fromMyFeed2ParticularTitle);
+        fromMyFeed2ParticularTitle.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == fromMyFeed2ParticularTitle){
+            ((main)getActivity()).onFragmentSelected(6,null);
+        }
     }
 }
