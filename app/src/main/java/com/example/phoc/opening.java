06adf346eeacf63361501d48e278053070aa05dc
HamInGyu <hfire1207@gmail.com>
2019-12-19 03:43:28 +0900

    opening 화면꾸미기

--- app/src/main/java/com/example/phoc/opening.java
+++ app/src/main/java/com/example/phoc/opening.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +12,7 @@ import com.example.phoc.MySession.MySession;
 
 public class opening extends AppCompatActivity implements View.OnClickListener {
 
-    Button startBtn;
+    RelativeLayout startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
